6ec42f2e08a608b9407b03000522e4fca9b4e4ad
BigBall3s <89066635+BigBall3s@users.noreply.github.com>
2021-08-18 02:51:08 -0400

    Update trash.js

--- trash.js
+++ trash.js
@@ -1,3 +1,15 @@
-$.getJSON("https://xbl.io/api/v2/account/2535440036473885?X-Authorization=gkwksgwsggws8cgs48okwooo4g8sc4g4ows", function(data){
-console.log(data)
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://xbl.io/api/v2/account/2535440036473885",
+  "method": "GET",
+  "headers": {
+    "x-authorization": "gkwksgwsggws8cgs48okwooo4g8sc4g4ows",
+    "cache-control": "no-cache",
+    "postman-token": "ac7b4f53-128d-508b-4861-a3b876abe71f"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
