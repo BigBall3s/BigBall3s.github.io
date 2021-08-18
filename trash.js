14b778aeb43505165362e31cfb5a44c2436758b3
BigBall3s <89066635+BigBall3s@users.noreply.github.com>
2021-08-18 03:09:07 -0400

    g

--- trash.js
+++ trash.js
@@ -1,15 +1,17 @@
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://xbl.io/api/v2/account/2535440036473885",
-  "method": "GET",
-  "headers": {
-    "x-authorization": "gkwksgwsggws8cgs48okwooo4g8sc4g4ows",
-    "cache-control": "no-cache",
-    "postman-token": "ac7b4f53-128d-508b-4861-a3b876abe71f"
-  }
-}
+var data = null;
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
 });
+
+xhr.open("GET", "https://xbl.io/api/v2/account/2535440036473885");
+xhr.setRequestHeader("x-authorization", "gkwksgwsggws8cgs48okwooo4g8sc4g4ows");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.setRequestHeader("postman-token", "eb60da24-1cf8-2c7b-081c-d76f01b56835");
+
+xhr.send(data);
