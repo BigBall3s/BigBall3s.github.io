7dc3440018e81ced0d5491ce311aa21fa9b54f28
BigBall3s <89066635+BigBall3s@users.noreply.github.com>
2021-08-18 03:14:05 -0400

    h

--- trash.js
+++ trash.js
@@ -1,17 +1,15 @@
-var data = null;
-
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function () {
-  if (this.readyState === 4) {
-    console.log(this.responseText);
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://xbl.io/api/v2/account/2535440036473885",
+  "method": "GET",
+  "headers": {
+    "x-authorization": "gkwksgwsggws8cgs48okwooo4g8sc4g4ows",
+    "cache-control": "no-cache",
+    "postman-token": "88836628-b4d0-be3a-6577-e848cf0e2a40"
   }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
 });
-
-xhr.open("GET", "https://xbl.io/api/v2/account/2535440036473885");
-xhr.setRequestHeader("x-authorization", "gkwksgwsggws8cgs48okwooo4g8sc4g4ows");
-xhr.setRequestHeader("cache-control", "no-cache");
-xhr.setRequestHeader("postman-token", "eb60da24-1cf8-2c7b-081c-d76f01b56835");
-
-xhr.send(data);
